370be8ff4d96d3c9518b482d6a6f9f568bff5c5b
Vasile Moisei <vas.kazaku@gmail.com>
2026-08-17 18:06:06 +0200

    Initial commit: Vite setup and project cleanup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function App() {
+  return (
+    <div className="app-container">
+      <h1>Sneaker Store este pregătit!</h1>
+    </div>
+  );
+}
+
+export default App;
